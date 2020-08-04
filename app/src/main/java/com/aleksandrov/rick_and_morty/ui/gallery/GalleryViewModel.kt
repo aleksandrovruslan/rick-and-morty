b49b64310886c2c7d1615a00fb87665f176fee21
Ruslan <aleksruslan@mail.ru>
2020-08-05 02:07:11 +0300

    init commit

---
+++ app/src/main/java/com/aleksandrov/rick_and_morty/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.aleksandrov.rick_and_morty.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
